80f6df3b661393d736095ed0d8d9505d048f39f0
[Surekshya Sharma] <[xst-shar3415@stthomas.edu]>
2019-07-02 15:52:40 -0500

    Created a Tip Calculator

---
+++ su.js
@@ -0,0 +1,51 @@
+$(document).ready( function () {
+   tipCalculator();
+  
+});
+function tipCalculator(){
+    console.log("let's get started");
+
+    tipPer = document.getElementById("tip").value;
+    totalAmount = document.getElementById("amount").value;
+    number = document.getElementById("people").value;
+
+    //converting string to float
+    var p = parseFloat(tipPer);
+    var a = parseFloat(totalAmount);
+    var n = parseFloat(number);
+
+        //console.log(a);
+
+    //checking the inputs
+     if (totalAmount == "")
+     {
+        alert("Enter the valid amount");
+     }
+     else if
+         (totalAmount == 0){
+            alert(" Enter the valid amount");
+     }
+     else if
+         (number == 0){
+            alert("Enter the valid number");
+     }else{
+
+                
+                tipCalculated = a /100 * p;
+
+                finalTip = tipCalculated /number;
+                //next line allows us to always have two digits after decimal point
+                finalTip = finalTip.toFixed(2);
+
+                console.log(finalTip);
+
+                    $("#calculation").append(
+                        'Your tip Amount is $ '+
+                                finalTip+ "."
+
+                        )
+                }
+     //console.log(finalTip);
+    }
+    
+    
